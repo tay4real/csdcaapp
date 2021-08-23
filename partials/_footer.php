861f6b02ee0a31ea93be48de2e91e4283e519679
Otubusin Ademuyiwa <53128146+tay4real@users.noreply.github.com>
2021-08-23 15:06:31 +0100

    Update
    Set up AdminLTE

---
+++ partials/_footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2021 <a href="https://adminlte.io"><img src="dist/img/csdc.png" alt="CSDC Logo" width="100"/></a>.</strong>
+    All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 1.0
+    </div>
+  </footer>
---
+++ partials/_included_scripts.php
@@ -0,0 +1,37 @@
+
+<!-- jQuery -->
+<script src="../csdcapp/plugins/jquery/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="../csdcapp/plugins/jquery-ui/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="../csdcapp/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- ChartJS -->
+<script src="../csdcapp/plugins/chart.js/Chart.min.js"></script>
+<!-- Sparkline -->
+<script src="../csdcapp/plugins/sparklines/sparkline.js"></script>
+<!-- JQVMap -->
+<script src="../csdcapp/plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="../csdcapp/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="../csdcapp/plugins/jquery-knob/jquery.knob.min.js"></script>
+<!-- daterangepicker -->
+<script src="../csdcapp/plugins/moment/moment.min.js"></script>
+<script src="../csdcapp/plugins/daterangepicker/daterangepicker.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="../csdcapp/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Summernote -->
+<script src="plugins/summernote/summernote-bs4.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="../csdcapp/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../csdcapp/dist/js/adminlte.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../csdcapp/dist/js/demo.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="../csdcapp/dist/js/pages/dashboard.js"></script>
+
+
